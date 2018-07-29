81ac2cf9c8d50bce165a6dc0a7a13fc3e269b306
Matheus Leal <matheusaquino@id.uff.br>
2018-07-29 09:37:13 -0300

    Adição descrição mapa

--- front/js/script.js
+++ front/js/script.js
@@ -8,21 +8,3 @@ function logout() {
 	document.cookie = "logged=false";
 	window.location.href = "index.html";
 }
-
-function getLocation() {
-  if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-      x.innerHTML = "Localização não é suportada pelo navegador!";
-      x.hidden = false;
-  }
-}
-
-function showPosition(position) {
-  latitude  = position.coords.latitude;
-  longitude = position.coords.longitude;
-}
-
-//var primeiraLatitude = ;
-//var segundaLatitude = ;
-
